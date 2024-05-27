330e43d292209a8b1bdc1a3fa9e44cd6e469c942
leningael <leningaelrs@gmail.com>
2024-05-27 16:40:11 -0600

    feat: :sparkles: Initial setup for elk stack

--- app/infrastructure/routers/management_router.py
+++ app/infrastructure/routers/management_router.py
@@ -2,18 +2,12 @@ from fastapi import APIRouter, status
 
 from app.application.services.metrics_service import MetricsService
 
-from app.infrastructure.dto.microservice_info_dto import MicroserviceInfoDTO
 from app.infrastructure.repositories.relational_database_driver_assignment_repository_impl import RelationalDatabaseDriverAssignmentRepositoryImpl
 from app.infrastructure.repositories.relational_database_driver_repository_impl import RelationalDatabaseDriverRepositoryImpl
 from app.infrastructure.repositories.relational_database_user_repository_impl import RelationalDatabaseUserRepositoryImpl
 from app.infrastructure.repositories.relational_database_vehicle_repository_impl import RelationalDatabaseVehicleRepositoryImpl
 
 management_router = APIRouter()
-
-
-@management_router.get("/info", status_code=status.HTTP_200_OK)
-def get_microservice_information() -> MicroserviceInfoDTO:
-    return MicroserviceInfoDTO()
 
 @management_router.get("/metrics",status_code=status.HTTP_200_OK)
 def get_metrics_information() :
