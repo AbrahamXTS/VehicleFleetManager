a66acdd92477aeaef8694bb4795bb4fbf154bab4
leningael <leningaelrs@gmail.com>
2024-05-28 20:13:30 -0600

    feat: :sparkles: Logs added for invitation code methods

--- app/application/services/invitation_code_service.py
+++ app/application/services/invitation_code_service.py
@@ -7,7 +7,7 @@ from app.domain.exceptions.conflict_with_existing_resource_exception import (
 )
 from app.domain.exceptions.resource_not_found_exception import ResourceNotFoundException
 from app.domain.models.invitation_code_model import InvitationCodeModel
-import logging
+from loguru import logger
 
 
 class InvitationCodeService:
@@ -18,28 +18,25 @@ class InvitationCodeService:
     ) -> None:
         self.invitation_code_repository = invitation_code_repository
         self.user_repository = user_repository
-        self.logger = logging.getLogger(__name__)
 
     def get_all_invitation_codes_by_user_id(
         self, user_id: int
     ) -> list[InvitationCodeModel]:
-        self.logger.info("Method get_all_invitation_codes_by_user_id()")
+        logger.debug("Method called: get_all_invitation_codes_by_user_id()")
+        logger.debug(f"Params passed: {user_id}")
         return self.user_repository.get_invitation_codes_created_by_user_id(user_id)
 
     def create_invitation_code(
         self, recipient_email: str, authenticated_user_id: int
     ) -> InvitationCodeModel:
-        self.logger.info("Method create_invitation_code()")
+        logger.debug("Method called: create_invitation_code()")
+        logger.debug(f"Params passed: {recipient_email} and {authenticated_user_id}")
         if self.user_repository.get_user_by_email(
             recipient_email
         ) or self.invitation_code_repository.get_invitation_code_by_email(
             recipient_email
         ):
-            self.logger.warning(
-                f"Conflict: User with email {recipient_email} already exists"
-            )
             raise ConflictWithExistingResourceException
-        self.logger.debug(f"Creating invitation code for email: {recipient_email}")
         return self.invitation_code_repository.save_invitation_code(
             InvitationCodeModel(
                 code=None, email=recipient_email, created_by_user=authenticated_user_id
@@ -49,14 +46,14 @@ class InvitationCodeService:
     def update_recipient_email_from_invitation_code(
         self, code: str, recipient_email: str, authenticated_user_id: int
     ) -> InvitationCodeModel:
-        self.logger.info("Method update_recipient_email_from_invitation_code()")
+        logger.debug("Method called: update_recipient_email_from_invitation_code()")
+        logger.debug(f"Params passed: {code}, {recipient_email} and {authenticated_user_id}")
         if code not in [
             invitation_code.code
             for invitation_code in self.user_repository.get_invitation_codes_created_by_user_id(
                 authenticated_user_id
             )
         ]:
-            self.logger.warning(f"Invitation code with code {code} not found")
             raise ResourceNotFoundException
 
         if self.user_repository.get_user_by_email(
@@ -64,12 +61,8 @@ class InvitationCodeService:
         ) or self.invitation_code_repository.get_invitation_code_by_email(
             recipient_email
         ):
-            self.logger.warning(
-                f"Conflict: User with email {recipient_email} already exists"
-            )
             raise ConflictWithExistingResourceException
 
-        self.logger.debug(f"Saving updated invitation code with code: {code}")
         return self.invitation_code_repository.save_invitation_code(
             InvitationCodeModel(
                 code=code, email=recipient_email, created_by_user=authenticated_user_id
@@ -79,14 +72,14 @@ class InvitationCodeService:
     def delete_invitation_code_by_code(
         self, code: str, authenticated_user_id: int
     ) -> None:
-        self.logger.info("Method delete_invitation_code_by_code()")
+        logger.debug("Method called: delete_invitation_code_by_code()")
+        logger.debug(f"Params passed: {code} and {authenticated_user_id}")
         if code not in [
             invitation_code.code
             for invitation_code in self.user_repository.get_invitation_codes_created_by_user_id(
                 authenticated_user_id
             )
         ]:
-            self.logger.warning(f"Invitation code with code {code} not found")
             raise ResourceNotFoundException
 
         return self.invitation_code_repository.delete_invitation_code_by_code(code)
--- app/infrastructure/repositories/relational_database_invitation_code_repository_impl.py
+++ app/infrastructure/repositories/relational_database_invitation_code_repository_impl.py
@@ -1,5 +1,5 @@
 from sqlmodel import Session, select
-import logging
+from loguru import logger
 
 from app.application.repositories.invitation_code_repository import (
     InvitationCodeRepository,
@@ -14,20 +14,15 @@ from app.infrastructure.mappers.invitation_code_mappers import (
 
 
 class RelationalDatabaseInvitationCodeRepositoryImpl(InvitationCodeRepository):
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
 
     def get_invitation_code_by_code(self, code: str) -> InvitationCodeModel | None:
-        self.logger.info(
-            f"Method get_invitation_code_by_code(), Invitation code with code {code}"
-        )
         with Session(db_engine) as session:
             invitation_code_entity = session.exec(
                 select(InvitationCode).where(InvitationCode.code == code)
             ).first()
 
             if invitation_code_entity:
-                self.logger.debug(f"Invitation code: {invitation_code_entity}")
+                logger.debug(f"Method get_invitation_code_by_code(), Retrieved invitation code with code: {code}")
                 return map_invitation_code_entity_to_invitation_code_model(
                     invitation_code_entity
                 )
@@ -39,10 +34,7 @@ class RelationalDatabaseInvitationCodeRepositoryImpl(InvitationCodeRepository):
             ).first()
 
             if invitation_code_entity:
-                self.logger.info(
-                    f"Method get_invitation_code_by_email() ,Invitation code with email {email}"
-                )
-                self.logger.debug(f"Invitation code: {invitation_code_entity}")
+                logger.debug(f"Method get_invitation_code_by_email(), Retrieved invitation code with email: {email}")
                 return map_invitation_code_entity_to_invitation_code_model(
                     invitation_code_entity
                 )
@@ -58,12 +50,7 @@ class RelationalDatabaseInvitationCodeRepositoryImpl(InvitationCodeRepository):
             ).first()
 
             if invitation_code_entity:
-                self.logger.info(
-                    f"Method get_invitation_code_by_code_and_email(), Invitation code with code {code} and email {email}"
-                )
-                self.logger.debug(
-                    f"Invitation code: {invitation_code_entity.email} {invitation_code_entity.code}"
-                )
+                logger.debug(f"Method get_invitation_code_by_code_and_email(), Retrieved invitation code with code {code} and email {email}")
                 return map_invitation_code_entity_to_invitation_code_model(
                     invitation_code_entity
                 )
@@ -71,9 +58,6 @@ class RelationalDatabaseInvitationCodeRepositoryImpl(InvitationCodeRepository):
     def save_invitation_code(
         self, invitation_code: InvitationCodeModel
     ) -> InvitationCodeModel:
-        self.logger.info(
-            f"Method save_invitation_code(), Saving invitation code: {invitation_code}"
-        )
         with Session(db_engine) as session:
             invitation_code_entity = None
 
@@ -93,17 +77,12 @@ class RelationalDatabaseInvitationCodeRepositoryImpl(InvitationCodeRepository):
             session.add(invitation_code_entity)
             session.commit()
             session.refresh(invitation_code_entity)
-            self.logger.debug(
-                f"Invitation code saved: {invitation_code_entity.email} {invitation_code_entity.code}"
-            )
+            logger.debug(f"Method save_invitation_code(), Invitation code: {invitation_code_entity.code} saved")
             return map_invitation_code_entity_to_invitation_code_model(
                 invitation_code_entity
             )
 
     def delete_invitation_code_by_code(self, code: str) -> None:
-        self.logger.info(
-            f"Method delete_invitation_code_by_code(), Deleting invitation code with code {code}"
-        )
         with Session(db_engine) as session:
             invitation_code_entity = session.exec(
                 select(InvitationCode).where(InvitationCode.code == code)
@@ -111,4 +90,4 @@ class RelationalDatabaseInvitationCodeRepositoryImpl(InvitationCodeRepository):
 
             session.delete(invitation_code_entity)
             session.commit()
-            self.logger.debug(f"Invitation code: {invitation_code_entity}")
+            logger.debug(f"Method delete_invitation_code_by_code(), Invitation code with code {code} deleted")
