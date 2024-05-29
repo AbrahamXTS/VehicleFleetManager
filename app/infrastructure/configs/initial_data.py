0da90953ae71be5256440d846c8972623d0c3202
leningael <leningaelrs@gmail.com>
2024-05-28 23:11:43 -0600

    feat: :sparkles: Expose of database port in dockercompose

--- app/infrastructure/configs/initial_data.py
+++ app/infrastructure/configs/initial_data.py
@@ -6,6 +6,6 @@ def add_default_user():
     with Session(db_engine) as session:
         if session.exec(select(User).where(User.email == "root@mail.com")).first():
             return
-        user = User(name="Root", last_name="Root", email="root@mail.com", password="$2b$12$AnFLK5tFAvnNnLSpBGjxYOCVP1uRgzTf2s7k4wmN2bkQYk9W3Wsoq")
+        user = User(name="Root", last_name="Root", email="root@mail.com", password="$2b$12$kuK09K2T9GWw5I6yeStKUesS5A3Xp6M.S91.H8h9tlWORPBYlJlgu")
         session.add(user)
         session.commit()
